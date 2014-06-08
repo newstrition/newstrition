c0a152771e987cbb9e1eea1ed68a306cf2c55910
adorsk <adorsk@gmail.com>
2014-06-08 09:49:30 -0400

    basic url parser, still need to add in to main code

--- fetchParserConfig/csvToParserConfig.py
+++ fetchParserConfig/csvToParserConfig.py
@@ -9,39 +9,43 @@ import json
 
 
 CSV_FILE = 'parserConfig.csv'
-CAT_START_IDX = 5
 
 def main():
     with open(CSV_FILE, 'rb') as csvfile:
         reader = csv.reader(csvfile)
         headers = reader.next()
+        cat_start_idx = headers.index('Category Labels') + 1
         cat_end_idx = len(headers) - 1
         
         source_configs = {}
         for row in reader:
+            if row[0] == 'SKIP':
+                break
             source_config = {}
             source_config['domain'] = row[0].replace("http://", "")
             source_config['label'] = row[1]
 
-            # Parse the url foramt. 
-            raw_url_format = row[2]
-            if raw_url_format.startswith('<FIXED:'):
-                 source_config['fixedCategory'] = re.search(
-                     '<FIXED:(.*)>', raw_url_format).group(1)
-            else:
-                source_config['urlFormat'] = raw_url_format.replace(
-                    '<category>', '(.*?)')
-                cat_map = {}
-                for i in range(CAT_START_IDX, cat_end_idx+1):
-                    source_cats = row[i]
-                    canonical_cat = headers[i]
-                    if source_cats:
-                        for source_cat in source_cats.split(','):
-                            source_cat = source_cat.strip()
-                            cat_map[source_cat] = canonical_cat
-                source_config['categoryMap'] = cat_map
+            if row[2]:
+                source_config['defaultCategory'] = row[2]
+
+            # Parse the category map.
+            cat_tests = []
+            base_path = row[3]
+            for i in range(cat_start_idx, cat_end_idx+1):
+                source_cats = row[i]
+                canonical_cat = headers[i]
+                if source_cats:
+                    for source_cat in source_cats.split(','):
+                        cat_re = base_path + source_cat.strip()
+                        cat_tests.append([cat_re, canonical_cat])
+            source_config['categoryTests'] = cat_tests
             source_configs[source_config['domain']] = source_config
-        print "var parserConfig = " + json.dumps(source_configs, indent=2)
+
+            # Include example urls for testing.
+            source_config['exampleUrls'] = row[4].split('\n')
+
+        # Print out parserConfig js.
+        print "var parserConfig = " + json.dumps(source_configs, indent=2) + ';'
 
 if __name__ == '__main__':
     main()
--- fetchParserConfig/generateParserConfig.sh
+++ fetchParserConfig/generateParserConfig.sh
@@ -2,4 +2,4 @@
 
 curl "https://docs.google.com/spreadsheets/d/17Q65IfosaiXfyoI5LaCmX2adFekf11Mzv2fWoOWanRo/export?format=csv&id=17Q65IfosaiXfyoI5LaCmX2adFekf11Mzv2fWoOWanRo&gid=0" > parserConfig.csv
 
-./csvToParserConfig.py > parserConfig.js
+./csvToParserConfig.py > ../newstrition/js/parserConfig.js
