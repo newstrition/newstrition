1b75c560b990e2e6556dcb43123ad743c8df4abf
Ying Quan Tan <ying.quan.tan@gmail.com>
2014-06-08 10:55:59 -0400

    updated manifest and js

--- newstrition/js/popup.js
+++ newstrition/js/popup.js
@@ -49,11 +49,63 @@ var newstrition = {
             //   };
             //   item.embedly = embedly;
         //});
-      filteredResults.push(item);
+        filteredResults.push(item);
+      }
     }
+    return filteredResults;
+  },
+
+  getTemplate : function () {
+    var sometemplate = '{{#stats}}<div><span class="title">{{title}}</span> <span class="num">{{percentage}}</span></div>{{/stats}}';
+    var source = sometemplate;
+    var template = Handlebars.compile(source); 
+    return template;
+  },
+
+  render: function (data) {
+    this.renderHtml(data);
+    this.renderChart(data.stats);
+  },
+
+  renderHtml : function(data) {
+    var template = this.getTemplate();
+    $('.content').append(template(data));
+  },
+
+  renderChart : function(data) {
+    (function(nv, d3, data) {
+      var colors = [
+        '#ff628e',
+        '#ff7777',
+        '#62dcff',
+        '#08647e'
+      ];
+      nv.addGraph(function() {
+        var chart = nv.models.pieChart()
+        .x(function(d) { return d.title; })
+        .y(function(d) { return d.percentage; })
+        .showLabels(false)
+        .showLegend(false)
+        .labelThreshold(0.5)
+        .donut(true)
+        .donutRatio(0.45)
+        .color(colors)
+        .margin({
+          top: 0,
+          right: 0,
+          bottom: 0,
+          left: 0
+        });
+
+        d3.select('svg')
+        .datum(data)
+        .transition().duration(500)
+        .call(chart); 
+
+        return chart;
+      });
+    })(nv, d3, data);
   }
-  return filteredResults;
-},
 
 }
 
@@ -64,130 +116,37 @@ newstrition.parser = {
   }
 }
 
+
 document.addEventListener('DOMContentLoaded', function () { //TODO: what is a better endpoint?
   console.log("still alive");
-
   var startTime = newstrition.getStartSearchTime(1); // get one day of browsing history
-
-/*
-var theTemplateScript = '{{#each}}{{title}}{{/each}}'; // (step 1)
-
-var theData =  [
-  {
-    "title" : "Politics",
-    "percentage" : 10.0,
-  },
-  {
-    "title" : "Sports",
-    "percentage" : 30.0,
-  },
-  {
-    "title" : "World",
-    "percentage" : 40.0,
-  },
-  {
-    "title" : "Art",
-    "percentage" : 20.0,
-  }
-];
-
-var theTemplate = Handlebars.compile(theTemplateScript);
-$('.content').append(theTemplate(theData));
-
-*/
-
-
-var sometemplate = '{{#stats}}<div class="item"><span class="title">{{title}}</span> <span class="num">{{percentage}}</span></div>{{/stats}}';
-var source = sometemplate;
-var template = Handlebars.compile(source); 
-
-var data = { 
-    stats: [
-  {
-    "title" : "Politics",
-    "percentage" : 10.0,
-  },
-  {
-    "title" : "Sports",
-    "percentage" : 30.0,
-  },
-  {
-    "title" : "World",
-    "percentage" : 40.0,
-  },
-  {
-    "title" : "Art",
-    "percentage" : 20.0,
-  }
-]
-}; 
-
-
-
-//$('.content').append(template(data));
-
-$('.line2').after(template(data))
-
-var data = [
-  {
-    'title' : 'Politics',
-    'percentage' : 10.0
-  },
-  {
-    'title' : 'Sports',
-    'percentage' : 30.0
-  },
-  {
-    'title' : 'World',
-    'percentage' : 40.0
-  },
-  {
-    'title' : 'Art',
-    'percentage' : 20.0
-  }
-];
-
-
-(function(nv, d3, data) {
-  var colors = [
-    '#ff628e',
-    '#ff7777',
-    '#62dcff',
-    '#08647e'
-  ];
-  nv.addGraph(function() {
-    var chart = nv.models.pieChart()
-      .x(function(d) { return d.title; })
-      .y(function(d) { return d.percentage; })
-      .showLabels(false)
-      .showLegend(false)
-      .labelThreshold(0.5)
-      .donut(true)
-      .donutRatio(0.45)
-      .color(colors)
-      .margin({
-        top: 0,
-        right: 0,
-        bottom: 0,
-        left: 0
-      });
-
-    d3.select('svg')
-      .datum(data)
-      .transition().duration(500)
-      .call(chart); 
-
-    return chart;
-  });
-})(nv, d3, data);
-
 
   // results is array of HistoryItem results
   chrome.history.search({ "text" : "", "startTime" : startTime, "maxResults" : 5 }, function (results) {
     console.log(newstrition.parser.parseResults(results));
     results = newstrition.getMetadata(results);
-    console.log(results);
+    var data = { 
+      stats: [
+        {
+          "title" : "Politics",
+          "percentage" : 10.0,
+        },
+        {
+          "title" : "Sports",
+          "percentage" : 30.0,
+        },
+        {
+          "title" : "World",
+          "percentage" : 40.0,
+        },
+        {
+          "title" : "Art",
+          "percentage" : 20.0,
+        }
+      ]
+    }; 
 
+    newstrition.render(data);
   });
 });
 
