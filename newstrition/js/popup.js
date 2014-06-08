60ee6c28c5def953d1780a5e55717442881cb48e
Kyle Goines <thirteenotherslikeme@gmail.com>
2014-06-07 21:58:46 -0400

    figured out handlebars tempting system

--- newstrition/js/popup.js
+++ newstrition/js/popup.js
@@ -21,10 +21,70 @@ document.addEventListener('DOMContentLoaded', function () { //TODO: what is a be
 
   var startTime = newstrition.getStartSearchTime(1); // get one day of browsing history
 
+/*
+var theTemplateScript = '{{#each}}{{title}}{{/each}}'; // (step 1)
+
+var theData =  [
+  {
+    "title" : "Politics",
+    "percentage" : 10.0,
+  },
+  {
+    "title" : "Sports",
+    "percentage" : 30.0,
+  },
+  {
+    "title" : "World",
+    "percentage" : 40.0,
+  },
+  {
+    "title" : "Art",
+    "percentage" : 20.0,
+  }
+];
+
+var theTemplate = Handlebars.compile(theTemplateScript);
+$('.content').append(theTemplate(theData));
+
+*/
+
+
+var sometemplate = '{{#stats}}{{title}}<br>{{percentage}}<br><br>{{/stats}}';
+var source = sometemplate;
+var template = Handlebars.compile(source); 
+
+var data = { 
+    stats: [
+  {
+    "title" : "Politics",
+    "percentage" : 10.0,
+  },
+  {
+    "title" : "Sports",
+    "percentage" : 30.0,
+  },
+  {
+    "title" : "World",
+    "percentage" : 40.0,
+  },
+  {
+    "title" : "Art",
+    "percentage" : 20.0,
+  }
+]
+}; 
+
+
+
+$('.content').html(template(data))
+
   // results is array of HistoryItem results
   chrome.history.search({ "text" : "", "startTime" : startTime, "maxResults" : 100 }, function (results) {
     console.log(newstrition.parser.parseResults(results));
   });
+
+
+
 
 });
 
